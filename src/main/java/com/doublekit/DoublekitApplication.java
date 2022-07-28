04fe5a79554153368d3dcb95bc912cab5cf1d8b9
zhangcheng <zhq11191750>
2022-07-28 15:35:24 +0800

    初始化

---
+++ src/main/java/com/doublekit/DoublekitApplication.java
@@ -0,0 +1,13 @@
+package com.doublekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoublekitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoublekitApplication.class, args);
+	}
+
+}
